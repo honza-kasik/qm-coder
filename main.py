220081cdecadab87667a5dfa84cb2d6c50f1e6e6
Honza Kasik <honza.kasik1@seznam.cz>
2017-06-24 12:42:04 +0200

    Added logging, limiting pointer length to 16 bits

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import bitarray
+import math
+import argparse
+import logging
+from ctypes import *
+
+logging.basicConfig(level=logging.DEBUG)
+hdlr = logging.FileHandler("log.txt")
+logger = logging.getLogger(__name__)
+logger.addHandler(hdlr)
+
 
 presets = [#q_e, mps_inc, lps_dec
     [0x59EB, 1, -1],
@@ -67,119 +77,215 @@ class ProbabilityTable:
     def is_interval_switch_needed(self):
         return presets[self._index][2] == -1
 
+class Encoder:
 
-class Coder:
-
-    def __init__(self, input, output):
-        self._input = input
-        self._output = output
+    def __init__(self, input_path, output_path):
+        self._input = bitarray.bitarray()
+        self._input.fromfile(open(input_path, 'rb'))
+        logger.debug("Loaded bitarray from file '%s' with endianity '%s' and lenght '%i'", input_path, self._input.endian(), len(self._input))
+        self._output_path = output_path
         self._p_table = ProbabilityTable()
-        self._a = 0xAAAA
-        self._c = 0x0000
-        self._excessive_bits = 15
-
+        self._a = c_uint16(0xb55a)
+        self._c = c_uint16(0x0000)
+        self._out = bitarray.bitarray()
 
     def encode(self):
-        #bits = [1,0,0,0,1,0,0,1,0,0,1,1,1,1,1,1,0,0,0,0,0] #100010010011111100000
-        bits = [1, 0, 1, 1, 1]
-        self._lps = 0
-        for bit in bits:
-            print("encoding", bit)
-            self._encode_bit(bit)
-        return (self._c, self._p_table.q_e(), self._lps, self._excessive_bits)
+        self._lps = 1
+        try:
+            while(True):
+                bit = self._input.pop()
+                logger.info("Encoding bit!")
+                self._encode_bit(bit)
+        except IndexError:
+            pass #ignored - end of bits to encode
+        self._out.append(1)
+        self._write_output(self._out)
 
     def _encode_bit(self, bit):
-        if bit == self._lps: #lps
-            self._c = self._c + (self._a - self._p_table.q_e())
-            self._a = self._p_table.q_e()
-            if self._p_table.is_interval_switch_needed():
-                self._swap_lps_mps()
-                self._p_table.next_mps()
-            else:
-                self._p_table.next_lps()
+        if bit == self._lps:
+            self._code_lps()
+        else:
+            self._code_mps()
+
+    def _q_e(self):
+        return self._p_table.q_e()
+
+    def _code_mps(self):
+        """
+        bit is more probable symbol, C is kept as is (bottom of MPS subinterval), interval A is shrunk to size of MPS
+        subinterval
+        """
+        self._a = c_uint16(self._a.value - self._q_e())
+        if self._a.value < 0x8000:
+            self._switch_intervals_if_needed()
             self._renormalize()
-        else: #mps
-            self._a = self._a - self._p_table.q_e()
             self._p_table.next_mps()
-            if self._a < 0x8000:
-                self._renormalize()
+        logger.debug("Coded MPS")
 
-    def _is_conditional_exchange_needed(self):
-        return self._p_table.is_interval_switch_needed()
-        #return self._a < self._p_table.q_e()
-
-    def _do_conditional_exchange_if_needed(self):
-        if self._is_conditional_exchange_needed():
-            self._swap_lps_mps()
+    def _code_lps(self):
+        """
+        bit is less probable symbol, bottom of LPS subinterval is added to C and interval A is shrunk to size of LPS
+        subinterval
+        :return:
+        """
+        self._c = c_uint16(self._c.value + self._a.value - self._q_e())
+        self._a = c_uint16(self._q_e())
+        self._switch_intervals_if_needed()
+        self._renormalize()
+        self._p_table.next_lps()
+        logger.debug("Coded LPS")
 
     def _renormalize(self):
-        while self._a < 0x8000:
-            self._a <<= 1
-            self._c <<= 1
-            self._excessive_bits += 1
+        while self._a.value < 0x8000:
+            #C < 0,5 (0xFFFF / 2)
+            logger.debug("C value is '%s'", hex(self._c.value))
+            logger.debug("A value is '%s'", hex(self._a.value))
+            if self._c .value< 0x5555:
+                b = 0
+                d = 0
+            else:
+                b = 1
+                d = 0x5555
+            self._write_bit(b)
+            logger.debug("Written '%i' to output", b)
+            #C = 2 * (C - D)
+            self._c = c_uint16((self._c.value - d) << 1)
+            logger.debug("C value is '%s'", hex(self._c.value))
+            #A = 2 * A
+            self._a = c_uint16(self._a.value << 1)
 
-    def _swap_lps_mps(self):
-        self._lps = self._get_mps()
+    def _switch_intervals_if_needed(self):
+        """
+        Conditional exchange if needed - if LPS subinterval becomes greater than LPS subinterval. This means, that
+        "less probable symbol" would be more probable than "more probable symbol", so switch takes place.
+        """
+        logger.debug("Should be switched according to values? '%s'", str(self._q_e() > self._a.value - self._q_e()))
+        if self._p_table.is_interval_switch_needed():
+            #self._a, self._c = self._c, self._a
+            self._lps = not(self._lps)
+            logger.debug("Switched intervals! LPS is now '%i'!", self._lps)
 
-    def _get_mps(self):
-        if self._lps == 1:
-            return 0
-        else:
-            return 1
+
+    def _write_bit(self, bit):
+        self._out.append(bit)
+
+    def _write_output(self, output):
+        with open(self._output_path, 'wb') as file:
+            output.tofile(file)
+
+class Decoder:
+
+    def __init__(self, input_path, output_path):
+        self._a = c_uint16(0xAAAA)
+        self._c = c_uint16(0x0000)
+        self._input = bitarray.bitarray()
+        self._input.fromfile(open(input_path, 'rb'))
+        self._output_path = output_path
+        self._p_table = ProbabilityTable()
+        self._out = bitarray.bitarray()
+        self._lps = 1
+        self._interval_pointer = c_uint16(self._load_int_from_bitarray(self._input))
+        logger.debug("Loaded '%i' as interval pointer, bin: '%s'", self._interval_pointer.value, str(bin(self._interval_pointer.value)))
 
     def decode(self):
-        self._c = self._input[0]
-        print("c:", self._c)
-
-        self._lps = self._input[2]
-        self._excessive_bits = self._input[3]
-
-        self._interval_pointer = self._c & 0x7fff #0x7fff - first 15 bits 111 1111 1111 1111
-        print("First interval pointer", self._interval_pointer)
-        self._c >>= 15
-        self._excessive_bits -= 15
-        output = bitarray.bitarray()
-        while(self._c > 0):
-            print("c:", self._c)
-            output.append(self._decode_bit())
-        return output
-
-    def _decode_bit(self):
-        dividing_line = self._a - self._p_table.q_e()
-        print("interval_pointer", self._interval_pointer, "dividing_line", dividing_line, "top_limit", self._a)
-        if self._interval_pointer > dividing_line: #points to the upper interval -> decoded lps
-            d = self._lps
-            self._interval_pointer = self._interval_pointer - (self._a - self._p_table.q_e())
-            self._a = self._p_table.q_e()
-            if self._is_conditional_exchange_needed():
-                self._swap_lps_mps()
-                self._p_table.next_mps()
+        while self._input.length() > 0:
+            dividing_line = self._a.value - self._q_e()
+            if self._interval_pointer.value > dividing_line:
+                self._decoded_lps()
+                logger.debug("Decoded LPS!")
             else:
-                self._p_table.next_lps()
-            self._renormalize_decode()
-        else: #points to the lower interval -> decoded mps
-            d = self._get_mps()
-            self._a = self._a - self._p_table.q_e()
-            self._p_table.next_mps()
-            if self._a < 0x8000:
-                self._renormalize_decode()
-        return d
+                self._decoded_mps()
+                logger.debug("Decoded MPS!")
+        self._write_to_file()
 
-    def _renormalize_decode(self):
-        while self._a < 0x8000:
-            self._a <<= 1
-            self._interval_pointer = (self._interval_pointer << 1) | (self._c & 1) #copy new bit from sequence to interval pointer
-            self._c >>= 1 #remove bit from sequence
-            self._excessive_bits -= 1
+    def _decoded_lps(self):
+        self._write_bit(self._lps)
+        self._c = c_uint16(self._c.value - self._a.value - self._q_e())
+        self._a = c_uint16(self._q_e())
+        self._renormalize()
+        self._p_table.next_lps()
+
+    def _decoded_mps(self):
+        self._write_bit(not(self._lps))
+        self._a = c_uint16(self._a.value - self._q_e())
+        if self._a.value < 0x8000:
+            self._renormalize()
+            self._p_table.next_mps()
+
+    def _renormalize(self):
+        if self._p_table.is_interval_switch_needed():
+            self._lps = not(self._lps)
+
+        logger.debug("C value is '%s'", hex(self._c.value))
+        logger.debug("A value is '%s'", hex(self._a.value))
+
+        while self._a.value < 0x8000:
+            if self._c.value < 0x5555:
+                d = 0
+            else:
+                d = 0x5555
+            #C = 2 * (C - D)
+            self._c = c_uint16((self._c.value - d) << 1)
+            #A = 2 * A
+            self._a = c_uint16(self._a.value << 1)
+            self._load_next_bit_to_interval_pointer(self._input, d)
+
+    def _load_next_bit_to_interval_pointer(self, bitarr: bitarray, d: int):
+        try:
+            b = bitarr.pop()
+        except IndexError:
+            b = 0
+        self._interval_pointer = c_uint16(((self._interval_pointer.value - d) << 1) + b)
+        logger.debug("Interval pointer is '%i'", self._interval_pointer.value)
+
+    def _load_int_from_bitarray(self, bitarr):
+        "Get x highest bits to form a bitarray"
+        c = 0
+        bit_count = self._rounded_log_base_2(self._q_e())
+        for i in range(bit_count, 0, -1):
+            top_bit = bitarr.pop()
+            logger.debug("Loading %i", top_bit)
+            c |= (top_bit << i)
+            logger.debug("New value: %s", str(bin(c)))
+        return c
+
+    def _q_e(self):
+        return self._p_table.q_e()
+
+    def _write_bit(self, bit):
+        self._out.append(bit)
+
+    def _write_to_file(self):
+        with open(self._output_path, 'wb') as file:
+            self._out.tofile(file)
+
+    def _rounded_log_base_2(self, number):
+        return int(math.ceil(math.log(number, 2)))
+
+def _encode(input_path, output_path):
+    coder = Encoder(input_path, output_path)
+    coder.encode()
+
+def _decode(input_path, output_path):
+    decoder = Decoder(input_path, output_path)
+    decoder.decode()
 
 def main():
-    coder = Coder("test.txt", "foo")
-    coded = coder.encode()
-    print(coded)
-    decoder = Coder(coded, "foo")
-    print("decoding: ", coded)
-    decoded = decoder.decode()
-    print(len(decoded), "'", decoded, "'")
+    parser = argparse.ArgumentParser(description='QM encoder/decoder')
+    parser.add_argument("mode", type=str, choices=["encode", "decode"],
+                        help="set mode, encode for encoding, decode for decoding")
+    parser.add_argument("input", type=str,
+                        help="path to input file")
+    parser.add_argument("output", type=str,
+                        help="path to output file")
+
+    args = parser.parse_args()
+
+    if args.mode == 'encode':
+         _encode(args.input, args.output)
+    else:
+        _decode(args.input, args.output)
+
 
 if __name__ == "__main__":
     main()
